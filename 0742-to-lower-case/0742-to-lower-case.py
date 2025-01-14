f5483f714eb9baa25cf02ab687dbe2621dec4df6
Ayush Kumar Srivastava <126005876+Ayush04H@users.noreply.github.com>
2025-01-14 11:01:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (29.69%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.py
+++ 0742-to-lower-case/0742-to-lower-case.py
@@ -1,4 +1,13 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        return s.lower()
+        lower_case = ""
+        for i in s:
+            ascii_val = ord(i)
+            if ascii_val >= 65 and ascii_val <= 90:
+                lower_case += chr(ascii_val + 32)
+            else:
+                lower_case += i
+
+        return lower_case
+
         
