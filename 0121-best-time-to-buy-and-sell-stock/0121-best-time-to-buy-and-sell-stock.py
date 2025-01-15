75ee1abc35f4062a493faf637a78a3ded95d5b63
Ayush Kumar Srivastava <126005876+Ayush04H@users.noreply.github.com>
2025-01-15 11:45:53 +0530

    Time: 86 ms (59.18%), Space: 26.9 MB (39.4%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,9 @@ class Solution:
         p = 0 
         bp = prices[0]
         n = len(prices)
-        for i in prices[1:]:
-            if bp > i:
-                bp = i
-            p = max(p,i-bp)
+        for i in range(1,n):
+            if bp > prices[i]:
+                bp = prices[i]
+            p = max(p,prices[i]-bp)
 
         return p
-        
