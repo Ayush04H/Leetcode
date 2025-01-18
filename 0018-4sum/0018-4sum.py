c34ff9b144d6366aa606a071481e8e593da76f1f
Ayush Kumar Srivastava <126005876+Ayush04H@users.noreply.github.com>
2025-01-18 13:01:36 +0530

    Time: 395 ms (54.25%), Space: 18 MB (22.16%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,27 +2,44 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         n = len(nums)
-        ans = set()
+        ans = []
 
         for i in range(n - 3):  # First element
+            # Skip duplicates for the first element
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
             for j in range(i + 1, n - 2):  # Second element
+                # Skip duplicates for the second element
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
                 l = j + 1  # Third element
                 r = n - 1  # Fourth element
 
                 while l < r:
                     s = nums[i] + nums[j] + nums[l] + nums[r]
-                    
+
                     if s == target:
-                        ans.add((nums[i], nums[j], nums[l], nums[r]))
+                        # Add the quadruplet to the result
+                        ans.append([nums[i], nums[j], nums[l], nums[r]])
+
+                        # Skip duplicates for the third element
+                        while l < r and nums[l] == nums[l + 1]:
+                            l += 1
+
+                        # Skip duplicates for the fourth element
+                        while l < r and nums[r] == nums[r - 1]:
+                            r -= 1
+
+                        # Move both pointers
                         l += 1
                         r -= 1
-                    
                     elif s < target:
                         l += 1
-                    
                     else:
                         r -= 1
-        
-        return [list(quadruplet) for quadruplet in ans]
+
+        return ans
                     
         
