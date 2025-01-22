7ac0c59ec4318a5a7d6952573dc0307dcae45d10
Ayush Kumar Srivastava <126005876+Ayush04H@users.noreply.github.com>
2025-01-22 12:00:09 +0530

    Time: 0 ms (100%), Space: 17.6 MB (48.27%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,16 +1,7 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        res = 0
+        count = 0
         while n > 0:
-            mod = n % 2
-            res = res * 10 + mod
-            n = n // 2
-        res = str(res)[::-1]
-
-        c = 0
-        for i in res:
-            if i == '1':  # Compare the current character, not the entire string
-                c += 1
-
-        return c
-        
+            n = n & (n - 1)  # Turn off the rightmost 1-bit
+            count += 1
+        return count
