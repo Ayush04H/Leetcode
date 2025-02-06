c45be10e0844ec83933c40971b4b57f7be2cedcb
Ayush Kumar Srivastava <126005876+Ayush04H@users.noreply.github.com>
2025-02-06 15:34:36 +0530

    Time: 335 ms (78.2%), Space: 46.4 MB (41.71%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,8 +1,14 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        count, ans, n = collections.Counter(), 0, len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                ans += 8 * count[nums[i]*nums[j]]
-                count[nums[i]*nums[j]] += 1
-        return ans
+        counts = {}
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] * nums[j] not in counts:
+                    counts[nums[i] * nums[j]] = 1
+                else:
+                    counts[nums[i] * nums[j]] += 1
+        res = 0
+        for freq in counts.values():
+            pairs = comb(freq, 2)
+            res += 8 * pairs
+        return res
